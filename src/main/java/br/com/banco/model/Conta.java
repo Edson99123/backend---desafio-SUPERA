a891d30afda88843e1f347de73390b3016a3b95b
Edson Sales <edsonsales99@gmail.com>
2022-12-16 13:42:16 -0400

    Criando estrutura do projeto

---
+++ src/main/java/br/com/banco/model/Conta.java
@@ -0,0 +1,9 @@
+package br.com.banco.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Conta {
+
+
+}
---
+++ src/main/java/br/com/banco/model/transferencia.java
@@ -0,0 +1,10 @@
+package br.com.banco.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+public class transferencia {
+
+
+}
